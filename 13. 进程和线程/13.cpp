2741296d4925f9c32ab5c1c3d349dc4435c2282e
YLHplus <93420580+luumod@users.noreply.github.com>
2022-10-27 11:54:29 +0800

    Add files via upload

---
+++ 13. 进程和线程/13.cpp
@@ -0,0 +1,23 @@
+﻿#include <Windows.h>
+
+int main()
+{
+	STARTUPINFOW sw{ 0 };
+	PROCESS_INFORMATION pInfo{ 0 };
+	BOOL Procerss = CreateProcessW(
+		/*要执行的模块名称*/L"F:\\windows编程\\1. 简单的窗口\\Debug\\1. 简单的窗口.exe",
+		/*要执行的命令行*/NULL,
+		/*可否由子进程继承*/NULL,
+		/*可否由子进程继承*/NULL,
+		/*是否可被继承*/FALSE,					
+		/*进程创建标志*/0,
+		/*指向新进程的环境块的指针*/NULL,
+		/*指定驱动器与工作目录*/NULL,
+		&sw,			//进程启动信息
+		&pInfo	//进程运行信息
+	);
+
+	CloseHandle(pInfo.hThread);
+	CloseHandle(pInfo.hProcess);
+	return 0;
+}
---
+++ 13. 进程相关操作/源.cpp
@@ -0,0 +1,71 @@
+﻿#include <Windows.h>
+#include <TlHelp32.h>
+#include <iostream>
+
+int main1()
+{
+	setlocale(LC_ALL, "chs");		//支持中文
+	//打开一个进程，获取进程句柄
+	HANDLE hProcress=OpenProcess(PROCESS_ALL_ACCESS, FALSE, 59300);
+	//结束进程
+	TerminateProcess(hProcress, 0);
+
+	/*
+	遍历进程
+	*/
+	//第二个参数只有在遍历堆或者模块的时候需要指定进程的ID
+#if 0
+	HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	PROCESSENTRY32W ProcessEntry32W{ sizeof(PROCESSENTRY32W)};
+	BOOL bSuccess = Process32FirstW(hSnapshot, &ProcessEntry32W);
+	if (bSuccess)
+	{
+		do
+		{
+			printf("进程ID: %d，进程名称: %ls\n", ProcessEntry32W.th32ProcessID, ProcessEntry32W.szExeFile);
+		} while (Process32NextW(hSnapshot, &ProcessEntry32W));
+	}
+#else
+	/*
+	遍历模块
+	*/
+	HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, 7524);
+	MODULEENTRY32W moduleEntry32W{ sizeof(MODULEENTRY32W) };
+	BOOL Getmodule32 = Module32FirstW(hSnapshot, &moduleEntry32W);
+	if (Getmodule32)
+	{
+		do
+		{
+			printf("模块ID: %d，模块名称: %ls\n", moduleEntry32W.th32ModuleID, moduleEntry32W.szExePath);
+		} while (Module32NextW(hSnapshot, &moduleEntry32W));
+	}
+#endif
+	return 0;
+}
+
+
+int main()
+{
+	/*
+	遍历当前计算机所有线程
+	*/
+	setlocale(LC_ALL, "chs");
+	HANDLE qqHandle = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, NULL);
+	THREADENTRY32 threadEntry32{ sizeof(THREADENTRY32) };
+	BOOL IsSuccess =  Thread32First(qqHandle, &threadEntry32);
+	UINT count = 0;
+	if (IsSuccess)
+	{
+		do
+		{
+			if (threadEntry32.th32OwnerProcessID == 33864)
+			{
+				printf("线程ID：%d\n", threadEntry32.th32ThreadID);
+				count++;
+			}
+		} while (Thread32Next(qqHandle, &threadEntry32));
+	}
+	printf("QQ所拥有的线程数:%d\n", count);
+	CloseHandle(qqHandle);
+	return 0;
+}
