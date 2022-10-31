a84a602f2a030dbc6f1044d9d4af7c9380823067
YLHplus <93420580+luumod@users.noreply.github.com>
2022-10-31 23:47:14 +0800

    Add files via upload

---
+++ 16. 邮槽/发送端.cpp
@@ -0,0 +1,37 @@
+﻿#include <Windows.h>
+
+int main()
+{
+	//往文件写入
+#if 0
+	HANDLE  hMailsolt = CreateFileW(L"\\\\.\\mailslot\\于良浩", GENERIC_WRITE, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+	if (hMailsolt == INVALID_HANDLE_VALUE)
+	{
+		MessageBoxW(NULL, L"打开邮槽失败", L"提示", MB_OK);
+		return 0;
+	}
+	WCHAR buff[] = L"6666666666666666";
+	DWORD size;
+	WriteFile(hMailsolt, buff, 10, &size, NULL);
+	CloseHandle(hMailsolt);
+#else
+	HANDLE Mailslot =  CreateFileW(L"//./mailslot/dst",
+		GENERIC_READ | GENERIC_WRITE,
+		FILE_SHARE_READ,
+		NULL,
+		OPEN_EXISTING,
+		FILE_ATTRIBUTE_NORMAL,
+		NULL);
+	if (Mailslot == INVALID_HANDLE_VALUE)
+	{
+		MessageBoxW(NULL, L"打开邮槽失败", L"提示", MB_OK);
+		return 0;
+	}
+	WCHAR lpBuff[] = L"我发送消息了";
+	DWORD CurSize = 0;
+	WriteFile(Mailslot, (LPCVOID)lpBuff, sizeof(lpBuff), &CurSize, NULL);
+	CloseHandle(Mailslot);
+
+#endif
+	return 0;
+}
---
+++ 16. 邮槽接收/接收区.cpp
@@ -0,0 +1,36 @@
+﻿#include <windows.h>
+
+int main()
+{
+#if 0
+	HANDLE Mailsolt = CreateMailslotW(L"\\\\.\\mailslot\\于良浩", NULL, MAILSLOT_WAIT_FOREVER, NULL);
+	if (Mailsolt == INVALID_HANDLE_VALUE)
+	{
+		MessageBoxW(NULL, L"创建邮槽失败", L"提示", MB_OK);
+	}
+	WCHAR buff[100]{ 0 };
+	DWORD size;
+	ReadFile(Mailsolt, buff, 100, &size, NULL);
+	MessageBoxW(NULL, buff, L"接收消息", MB_OK);
+	CloseHandle(Mailsolt);
+#else
+	HANDLE MailslotHandle =  CreateMailslotW(L"//./mailslot/dst", NULL, MAILSLOT_WAIT_FOREVER, NULL);
+	if (MailslotHandle == INVALID_HANDLE_VALUE)
+	{
+		MessageBoxW(NULL, L"创建邮槽失败!", L"提示", MB_OK);
+	}	
+	WCHAR lpBuff[100]{ 0 };
+	DWORD CurSize = 0;
+	while (1)
+	{
+		if (ReadFile(MailslotHandle, (LPVOID)lpBuff, 100, &CurSize, NULL))
+		{
+			MessageBoxW(NULL, lpBuff, L"接收消息", MB_OK);
+		}
+		memset(lpBuff, 0, sizeof(lpBuff));
+		CurSize = 0;
+	} ;
+	CloseHandle(MailslotHandle);
+#endif
+	return 0;
+}
