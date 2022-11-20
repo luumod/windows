656492cd79ea1caec329b18cf0e9359fa41ce6f7
YLHplus <93420580+luumod@users.noreply.github.com>
2022-11-20 23:51:06 +0800

    Add files via upload

---
+++ 22. 异步IO (1)/22.cpp
@@ -0,0 +1,39 @@
+﻿#include <Windows.h>
+#include <iostream>
+int main()
+{
+	//打开一个文件
+	HANDLE file1 = CreateFile(
+		L"test.txt",
+		GENERIC_READ,
+		FILE_SHARE_READ,
+		NULL,
+		OPEN_EXISTING,
+		FILE_FLAG_OVERLAPPED,
+		NULL
+	);
+	//OVERLAPPED结构体专门用来存储异步IO的内容
+	OVERLAPPED overlapped{ 0 };
+	CHAR buff[0x100]{ 0 };
+	//读取的字节要设置为NULL
+	ReadFile(file1,
+		buff,
+		0x100,
+		NULL,
+		&overlapped
+	);
+	/*
+	........在此处可以添加其他代码..........
+	*/
+	//等待file1是否已经读取完，是否是有信号状态（表示线程已经结束，即读取完毕）
+	WaitForSingleObject(file1, -1);	
+	DWORD CurSize = 0;
+	//读取是否成功
+	if (GetOverlappedResult(file1, &overlapped, &CurSize, TRUE))//False：不会等待，True：一直等待
+	{
+		printf("文件内容: %s\n", buff);
+		printf("实际读写数量: %d\n", CurSize);
+	}
+	CloseHandle(file1);
+	return 0;
+}
---
+++ 22. 异步IO (2)/22.2.cpp
@@ -0,0 +1,30 @@
+﻿#include <Windows.h>
+#include <iostream>
+
+int main()
+{
+	HANDLE File1 = CreateFileW(L"test.txt", GENERIC_READ | GENERIC_WRITE, NULL, NULL, OPEN_EXISTING, FILE_FLAG_OVERLAPPED, NULL);
+	OVERLAPPED overlapped1{ 0 };
+	OVERLAPPED overlapped2{ 0 };
+
+	overlapped1.hEvent = CreateEventW(NULL, FALSE, FALSE, L"FileEvent1");
+	overlapped2.hEvent = CreateEventW(NULL, FALSE, FALSE, L"FileEvent2");
+
+	WCHAR buff1[100]{ 0 };
+	WCHAR buff2[100]{ 0 };
+
+	ReadFile(File1, buff1, 100, NULL/*异步IO此处无意义*/, &overlapped1);
+	ReadFile(File1, buff2, 100, NULL, &overlapped2);
+
+	/*
+	...
+	*/
+
+	//仍然有阻塞状态
+	WaitForSingleObject(overlapped1.hEvent/*判断事件*/, -1);
+	WaitForSingleObject(overlapped2.hEvent/*判断事件*/, -1);
+	printf("buff1:%s\n", buff1);
+	printf("buff2:%s\n", buff2);
+
+	return 0;
+}
